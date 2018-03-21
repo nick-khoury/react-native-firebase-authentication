21b1c4ac00e4db24a43a90afdd3ada88ea5d962c
Nick_Khoury <nick.khoury@infosys.com>
2018-03-21 12:29:45 -0700

    added a piece of state to app component to know if user is logged in or not

--- src/App.js
+++ src/App.js
@@ -8,8 +8,10 @@ import LoginForm from './components/LoginForm';
 
 class App extends Component {
 
-    componentWillMount() {
-        //lifecycle method
+    state = { loggedIn: false };
+
+    componentWillMount() { //lifecycle method
+
         firebase.initializeApp({
             apiKey: "AIzaSyC13sjQ7gSK7fx8SX2pAm49ww3zmmFlMPg",
             authDomain: "authentication-7dacd.firebaseapp.com",
@@ -17,6 +19,15 @@ class App extends Component {
             projectId: "authentication-7dacd",
             storageBucket: "authentication-7dacd.appspot.com",
             messagingSenderId: "550786282586"
+        });
+
+        firebase.auth().onAuthStateChanged((user) => { // called when user signs in or out
+            // user is null when signing out
+            if (user) {
+                this.setState({ loggedIn: true });
+            } else {
+                this.setState({ loggedIn: false });
+            }
         });
     }
 
